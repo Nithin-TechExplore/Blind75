616754a6eb3ffea57da9a6f78f3b9f7a5be2c64d
Nithin N <60309802+Nithin-TechExplore@users.noreply.github.com>
2026-07-28 08:55:41 +0530

    LeetCode 217 Contains Duplicate Solved

---
+++ 217_ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class 217_ContainsDuplicate
+{
+    public static void main(String[] args)
+    {
+        List<Integer> li=Arrays.asList(1,2,3,4,3,5,6);
+        boolean ans=solve(li);
+
+
+    }
+
+    public static boolean solve(List<Integer> li)
+    {
+
+        HashSet<Integer> hs=new HashSet();
+
+        for(int i=0;i<li.size();i++)
+        {
+            if(hs.contains(li.get(i)))
+                return true;
+
+            hs.add(li.get(i));
+        }
+
+        return false;
+    }
+}
