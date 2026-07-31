e38142e492428fbfb19c23d0d94ec6736baf8982
Nithin N <60309802+Nithin-TechExplore@users.noreply.github.com>
2026-07-31 03:40:07 +0000

    JumpGame2_45

---
+++ JumpGame2_45.java
@@ -0,0 +1,36 @@
+public class JumpGame2_45
+{
+    public static void main(String[] args)
+    {
+
+        int[] nums={2,4,1,2,3,1,1,2};
+        int totalJumps = solve(nums);
+        System.out.println(totalJumps);
+    }
+
+    public static int solve(int[] nums)
+    {
+        int totalJumps=0;
+        int destination=nums.length-1;
+        int coverage=0,lastJumpIdx=0;
+
+        if(nums.length == 1) return 0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            coverage=Math.max(coverage,i+nums[i]);
+            if(i==lastJumpIdx)
+            {
+                lastJumpIdx=coverage;
+                totalJumps++;
+
+                if(coverage>=destination)
+                    return totalJumps;
+            }
+        }
+
+        return totalJumps;
+
+    }
+
+}
